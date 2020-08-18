674503e65b2ca695622f0efc549a560d40041a41
po987741 <69183325+po987741@users.noreply.github.com>
2020-08-18 11:15:17 +0700

    sent by Mr.Wongwat muadsri

---
+++ my c601.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void message();
+void cat();    
+
+void main(){
+    int animal=5;
+    printf("Hello World\n");
+    printf("%1",animal);
+    message();
+    cat();
+}
---
+++ myc602.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a=5;
+    int b=10;
+    int c=20;
+    int total = a+b+c;
+    printf("%i\n",total);
+}
